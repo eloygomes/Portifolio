48965b0470807f66da1d77241bc450fc7fe5f152
eloygomes <50409591+eloygomes@users.noreply.github.com>
2023-05-27 18:56:08 -0300

    update 0.6.1.0
    - pre tests with frame motion

--- my-project/src/Component/BioSession.jsx
+++ my-project/src/Component/BioSession.jsx
@@ -4,18 +4,25 @@ function BioSession() {
       <div className="bg-[#2A2344] w-screen h-screen flex flex-col" id="bio">
         <div className="container mx-auto mt-28">
           <div className="flex flex-row">
-            <div className="w-[50%] h-screen flex flex-col mx-auto">
+            <div className="w
+            -[50%] h-screen flex flex-col mx-auto">
               <div className="w-fit mt-52"></div>
-              
-              <h5 className="py-1 text-sm max-w-lg">
-                Olá! Sou um desenvolvedor Full Stack com 15 anos de experiência.
+              <h4 className="mx-1">Olá!, Sou um... </h4>
+              <h1 className="py-1 text-8xl font-medium hover:text-[#D6223B]"> Desenvolvedor<br/> Full Stack</h1>
+              <h3 className="bg-[#D6223B] text-xl w-fit">15 anos de experiência.</h3>
+              <h5 className="py-3 text-sm max-w-lg">
                 Iniciei da minha jornada como designer UI/UX, criando interfaces
                 de produtos e propondo melhorias de usabilidade na experiência
-                do usuário. Também tenho vivencia em criação de wireframes de
-                baixa/alta fidelidade e em desenvolvimento de protótipos
-                navegáveis. Hoje atuo como Full Stack Developer, utilizo
-                principalmente HTML5, CSS3, React Js e Wordpress para dar vida a
-                interfaces web dos projetos.
+                do usuário.
+              </h5>
+              <h5 className="py-3 text-sm max-w-lg">
+                Também tenho vivencia em criação de wireframes de baixa/alta
+                fidelidade e em desenvolvimento de protótipos navegáveis.
+              </h5>
+              <h5 className="py-3 text-sm max-w-lg">
+                Hoje atuo como Full Stack Developer, utilizo principalmente
+                HTML5, CSS3, React Js e Wordpress para dar vida a interfaces web
+                dos projetos.
               </h5>
             </div>
             <div className="w-[50%] mt-24 flex flex-col">
