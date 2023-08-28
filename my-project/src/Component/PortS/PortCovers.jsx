cfafa85fa17827b12c401c6fc95d9fc03c659326
eloygomes <50409591+eloygomes@users.noreply.github.com>
2023-08-28 19:07:04 -0300

    update 0.10.4.0
    - content updated of port session
    
    NEXT-->
    
    - keep upload material

--- my-project/src/Component/PortS/PortCovers.jsx
+++ my-project/src/Component/PortS/PortCovers.jsx
@@ -19,8 +19,8 @@ const images = {
     img6: "https://images.unsplash.com/photo-1598719830762-055a6665a463?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=987&q=80",
   },
   branding: {
-    img1: "https://images.unsplash.com/photo-1542596594-649edbc13630?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=987&q=80",
-    img2: "https://images.unsplash.com/photo-1453175324447-6864b23ecf23?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=987&q=80",
+    img1: "https://www.koncretejungle.com.br/eloygomes/assets/port/branding/galdino/%5B%20presentation%20%5D%20GALDINO.png",
+    img2: "https://www.koncretejungle.com.br/eloygomes/assets/port/branding/docpay/%5B%20presentation%20%5D%20docpay%2001.png",
     img3: "https://images.unsplash.com/photo-1520466809213-7b9a56adcd45?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=987&q=80",
     img4: "https://images.unsplash.com/photo-1524046346361-5a9c9592fb74?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1036&q=80",
     img5: "https://images.unsplash.com/photo-1648550733986-22e9139d6533?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=987&q=80",
--- my-project/src/Component/PortS/PortImages.jsx
+++ my-project/src/Component/PortS/PortImages.jsx
@@ -52,7 +52,7 @@ export const PortImages = {
       "https://www.koncretejungle.com.br/eloygomes/assets/port/ui%20ux/wellness%20hub/mockup_wellnessHUB_04.png",
       "https://www.koncretejungle.com.br/eloygomes/assets/port/ui%20ux/wellness%20hub/mockup_wellnessHUB_05.png",
     ],
-    // SEEKING 4 ELEMENTS UI UX 
+    // SEEKING 4 ELEMENTS UI UX
     container06: [
       "https://www.koncretejungle.com.br/eloygomes/assets/port/ui%20ux/seeking4elements/seeking4elements_mockup_01.png",
       "https://www.koncretejungle.com.br/eloygomes/assets/port/ui%20ux/seeking4elements/seeking4elements_mockup_02.png",
@@ -64,12 +64,11 @@ export const PortImages = {
     ],
   },
   impressos: {
-    // TAPERA  
+    // TAPERA
     container01: [
-        "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/tapera/tapera_02.png",
-        "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/tapera/tapera_03.png",
-        "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/tapera/tapera_01.png",
-      
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/tapera/tapera_02.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/tapera/tapera_03.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/tapera/tapera_01.png",
     ],
     // MANSOR SITE
     container02: [
@@ -79,9 +78,8 @@ export const PortImages = {
       "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/mansor/mansor_mockup_04.png",
       "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/mansor/mansor_mockup_05.png",
       "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/mansor/mansor_mockup_06.png",
-
     ],
-    // CATALOGO APONTA FACIL 
+    // CATALOGO APONTA FACIL
     container03: [
       "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/grupo%20skill/mockup_01.png",
       "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/grupo%20skill/mockup_02.png",
@@ -90,9 +88,8 @@ export const PortImages = {
       "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/grupo%20skill/mockup_05.png",
       "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/grupo%20skill/mockup_06.png",
       "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/grupo%20skill/mockup_07.png",
-      
     ],
-    // CATALOGO COLETEK 
+    // CATALOGO COLETEK
     container04: [
       "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/miroa/01.jpg",
       "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/miroa/02.jpg",
@@ -100,7 +97,172 @@ export const PortImages = {
       "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/miroa/04.jpg",
       "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/miroa/05.jpg",
       "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/miroa/06.jpg",
+    ],
+    container05: [
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/ui%20ux/wellness%20hub/mockup_wellnessHUB_00.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/ui%20ux/wellness%20hub/mockup_wellnessHUB_01.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/ui%20ux/wellness%20hub/mockup_wellnessHUB_02.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/ui%20ux/wellness%20hub/mockup_wellnessHUB_03.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/ui%20ux/wellness%20hub/mockup_wellnessHUB_04.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/ui%20ux/wellness%20hub/mockup_wellnessHUB_05.png",
+    ],
+    container06: [
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/ui%20ux/seeking4elements/seeking4elements_mockup_01.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/ui%20ux/seeking4elements/seeking4elements_mockup_02.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/ui%20ux/seeking4elements/seeking4elements_mockup_03.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/ui%20ux/seeking4elements/seeking4elements_mockup_04.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/ui%20ux/seeking4elements/seeking4elements_mockup_05.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/ui%20ux/seeking4elements/seeking4elements_mockup_06.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/ui%20ux/seeking4elements/seeking4elements_mockup_07.png",
+    ],
+  },
+  branding: {
+    // GALDINO
+    container01: [
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/branding/galdino/%5B%20presentation%20%5D%20GALDINO.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/branding/galdino/%5B%20mockup%20%5D%20GALDINO%2001.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/branding/galdino/%5B%20mockup%20%5D%20GALDINO%2002.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/branding/galdino/%5B%20mockup%20%5D%20GALDINO%2003.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/branding/galdino/%5B%20mockup%20%5D%20GALDINO%2004.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/branding/galdino/%5B%20mockup%20%5D%20GALDINO%2005.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/branding/galdino/%5B%20mockup%20%5D%20GALDINO%2006.png",
       
+    ],
+    // DOCPAY
+    container02: [
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/branding/docpay/%5B%20presentation%20%5D%20docpay%2001.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/branding/docpay/%5B%20mockup%20%5D%20docpay%2001.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/branding/docpay/%5B%20mockup%20%5D%20docpay%2002.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/branding/docpay/%5B%20mockup%20%5D%20docpay%2003.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/branding/docpay/%5B%20mockup%20%5D%20docpay%2004.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/branding/docpay/%5B%20mockup%20%5D%20docpay%2005.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/branding/docpay/%5B%20mockup%20%5D%20docpay%2006.png",
+      
+    ],
+    //
+    container03: [
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/grupo%20skill/mockup_01.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/grupo%20skill/mockup_02.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/grupo%20skill/mockup_03.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/grupo%20skill/mockup_04.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/grupo%20skill/mockup_05.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/grupo%20skill/mockup_06.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/grupo%20skill/mockup_07.png",
+    ],
+    //
+    container04: [
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/miroa/01.jpg",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/miroa/02.jpg",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/miroa/03.jpg",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/miroa/04.jpg",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/miroa/05.jpg",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/miroa/06.jpg",
+    ],
+    container05: [
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/ui%20ux/wellness%20hub/mockup_wellnessHUB_00.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/ui%20ux/wellness%20hub/mockup_wellnessHUB_01.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/ui%20ux/wellness%20hub/mockup_wellnessHUB_02.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/ui%20ux/wellness%20hub/mockup_wellnessHUB_03.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/ui%20ux/wellness%20hub/mockup_wellnessHUB_04.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/ui%20ux/wellness%20hub/mockup_wellnessHUB_05.png",
+    ],
+    container06: [
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/ui%20ux/seeking4elements/seeking4elements_mockup_01.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/ui%20ux/seeking4elements/seeking4elements_mockup_02.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/ui%20ux/seeking4elements/seeking4elements_mockup_03.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/ui%20ux/seeking4elements/seeking4elements_mockup_04.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/ui%20ux/seeking4elements/seeking4elements_mockup_05.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/ui%20ux/seeking4elements/seeking4elements_mockup_06.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/ui%20ux/seeking4elements/seeking4elements_mockup_07.png",
+    ],
+  },
+  foto: {
+    // GALDINO
+    container01: [
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/tapera/tapera_02.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/tapera/tapera_03.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/tapera/tapera_01.png",
+    ],
+    //
+    container02: [
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/mansor/mansor_mockup_01.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/mansor/mansor_mockup_02.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/mansor/mansor_mockup_03.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/mansor/mansor_mockup_04.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/mansor/mansor_mockup_05.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/mansor/mansor_mockup_06.png",
+    ],
+    //
+    container03: [
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/grupo%20skill/mockup_01.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/grupo%20skill/mockup_02.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/grupo%20skill/mockup_03.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/grupo%20skill/mockup_04.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/grupo%20skill/mockup_05.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/grupo%20skill/mockup_06.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/grupo%20skill/mockup_07.png",
+    ],
+    //
+    container04: [
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/miroa/01.jpg",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/miroa/02.jpg",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/miroa/03.jpg",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/miroa/04.jpg",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/miroa/05.jpg",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/miroa/06.jpg",
+    ],
+    container05: [
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/ui%20ux/wellness%20hub/mockup_wellnessHUB_00.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/ui%20ux/wellness%20hub/mockup_wellnessHUB_01.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/ui%20ux/wellness%20hub/mockup_wellnessHUB_02.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/ui%20ux/wellness%20hub/mockup_wellnessHUB_03.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/ui%20ux/wellness%20hub/mockup_wellnessHUB_04.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/ui%20ux/wellness%20hub/mockup_wellnessHUB_05.png",
+    ],
+    container06: [
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/ui%20ux/seeking4elements/seeking4elements_mockup_01.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/ui%20ux/seeking4elements/seeking4elements_mockup_02.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/ui%20ux/seeking4elements/seeking4elements_mockup_03.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/ui%20ux/seeking4elements/seeking4elements_mockup_04.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/ui%20ux/seeking4elements/seeking4elements_mockup_05.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/ui%20ux/seeking4elements/seeking4elements_mockup_06.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/ui%20ux/seeking4elements/seeking4elements_mockup_07.png",
+    ],
+  },
+  edicao: {
+    // GALDINO
+    container01: [
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/tapera/tapera_02.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/tapera/tapera_03.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/tapera/tapera_01.png",
+    ],
+    //
+    container02: [
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/mansor/mansor_mockup_01.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/mansor/mansor_mockup_02.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/mansor/mansor_mockup_03.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/mansor/mansor_mockup_04.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/mansor/mansor_mockup_05.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/mansor/mansor_mockup_06.png",
+    ],
+    //
+    container03: [
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/grupo%20skill/mockup_01.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/grupo%20skill/mockup_02.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/grupo%20skill/mockup_03.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/grupo%20skill/mockup_04.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/grupo%20skill/mockup_05.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/grupo%20skill/mockup_06.png",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/grupo%20skill/mockup_07.png",
+    ],
+    //
+    container04: [
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/miroa/01.jpg",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/miroa/02.jpg",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/miroa/03.jpg",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/miroa/04.jpg",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/miroa/05.jpg",
+      "https://www.koncretejungle.com.br/eloygomes/assets/port/impressos/miroa/06.jpg",
     ],
     container05: [
       "https://www.koncretejungle.com.br/eloygomes/assets/port/ui%20ux/wellness%20hub/mockup_wellnessHUB_00.png",
