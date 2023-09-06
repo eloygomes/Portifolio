e2e7fa5989cf79ff40f039926c3ab392b53a1d92
eloygomes <50409591+eloygomes@users.noreply.github.com>
2023-09-06 18:54:45 -0300

    update 0.10.17.7
    .

--- my-project/src/Component/PortS/PortCovers.jsx
+++ my-project/src/Component/PortS/PortCovers.jsx
@@ -35,12 +35,12 @@ const images = {
     img6: "https://www.koncretejungle.com.br/eloygomes/assets/port/portCovers/04%20-%20foto/foto01.png",
   },
   edicao: {
-    img1: "https://images.unsplash.com/photo-1542596594-649edbc13630?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=987&q=80",
-    img2: "https://images.unsplash.com/photo-1453175324447-6864b23ecf23?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=987&q=80",
-    img3: "https://images.unsplash.com/photo-1520466809213-7b9a56adcd45?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=987&q=80",
-    img4: "https://images.unsplash.com/photo-1524046346361-5a9c9592fb74?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1036&q=80",
-    img5: "https://images.unsplash.com/photo-1648550733986-22e9139d6533?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=987&q=80",
-    img6: "https://images.unsplash.com/photo-1598719830762-055a6665a463?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=987&q=80",
+    img1: "https://www.koncretejungle.com.br/eloygomes/assets/port/portCovers/05%20-%20edicao/edicao06.png",
+    img2: "https://www.koncretejungle.com.br/eloygomes/assets/port/portCovers/05%20-%20edicao/edicao05.png",
+    img3: "https://www.koncretejungle.com.br/eloygomes/assets/port/portCovers/05%20-%20edicao/edicao04.png",
+    img4: "https://www.koncretejungle.com.br/eloygomes/assets/port/portCovers/05%20-%20edicao/edicao03.png",
+    img5: "https://www.koncretejungle.com.br/eloygomes/assets/port/portCovers/05%20-%20edicao/edicao02.png",
+    img6: "https://www.koncretejungle.com.br/eloygomes/assets/port/portCovers/05%20-%20edicao/edicao01.png",
   },
 };
 
